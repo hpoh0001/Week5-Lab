3d0a19c92e6af11bfaa779609e4b17ae0429087e
Poh Hui Wen <huiwen@dyn-118-138-95-116.its.monash.edu.au>
2019-08-29 10:13:33 +1000

    commit for week 5 lab

---
+++ play.js
@@ -0,0 +1,58 @@
+var express= require("express");
+var bodyParser= require("body-parser");
+
+//instantiate express instance; 
+let app = express();
+
+// Express should be able to render ejs templates
+app.engine("html",require("ejs").renderFile);
+app.set("view engine","html");
+
+
+app.listen(8080);
+
+//use other files, have to be in the same folder
+app.use(express.static("images"));
+app.use(express.static("views"));
+app.use(express.static("css"));
+
+//allow Express to understand the urlencoded format
+app.use(bodyParser.urlencoded({
+    extended:false
+}))
+
+var db=[];
+
+db.push({
+    taskName:'task one',
+    taskDue: '21/8/2019',
+    taskDesc:"HW"
+});
+
+// viewPath is required for the response.sendFile function
+//__dirname is the  directory name of the current module (i.e file/project).
+let showView = __dirname+"/views/"
+
+//home page
+app.get("/",function(req,res){
+    res.sendFile(showView + "index.html");
+});
+
+//add new task page
+app.get("/addNewTask",function(req,res){
+    res.sendFile(showView + "newTasks.html");
+});
+
+app.post("/addTasks",function(req,res){
+    console.log(req.body);
+    db.push(req.body);
+    res.sendFile(showView + "index.html");
+});
+
+//list all tasks page
+app.get("/listAllTasks",function(req,res){
+    res.render(showView + "listTask.html",{data:db});
+});
+
+
+
